2dacc61e3e337b2ead76498400cf679ec742854b
Adam Walls <adam.walls@uky.edu>
2017-09-14 23:16:06 -0400

    Initial commit

---
+++ run_junit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+java -cp .:./org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar:./junit/4.12/junit-4.12.jar:./target/classes:./target/test-classes org.junit.runner.JUnitCore MyStackTest
