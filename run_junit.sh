4502b61fa32b86885c15b13415903b06ed31207b
Adam Walls <adam.walls@uky.edu>
2017-09-30 18:55:16 -0400

    Test

--- run_junit.sh
+++ run_junit.sh
@@ -1,3 +1,3 @@
-#!/bin/bash 
+#!/bin/bash
 
 java -cp .:./org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar:./junit/4.12/junit-4.12.jar:./target/classes:./target/test-classes org.junit.runner.JUnitCore MyStackTest
